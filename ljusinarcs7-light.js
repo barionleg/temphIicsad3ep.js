f90dee6b9de599a97085cef623dffbcdb2e16d39
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2026-06-21 18:56:24 +0200

    Create ljusinarcs7-light.js

---
+++ ljusinarcs7-light.js
@@ -0,0 +1,22 @@
+body {
+  position: absolute;
+  left: 200px;
+  top: -100px;
+
+}
+.path--background {
+  fill: none;
+  stroke: D00000;
+  stroke-width: 2px;
+}
+
+.path--foreground {
+  fill: rgb(0, 0, 208, 0.41);
+  color: ryb(0, 0, 208, 0.74);
+  }
+
+.label {
+  font: 14px sans-serif;
+  text-anchor: middle;
+  color: #fff;
+}
